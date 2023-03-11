d681fd2b1c55e3f43d1a466adfeac8db13e7648a
nodrjon <nodirjontolipov76@gmail.com>
2023-03-11 14:39:24 +0500

    Home

---
+++ src/componets/dropdowns/dpopdown2.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Dpopdown2() {
+    return (
+        <div>
+            <select id='select_laungare' className="form-select" aria-label="Default select example">
+                <option value="1">Us Dollarr</option>
+                <option value="2">Rus Ruble</option>
+                <option value="3">Uzb UZS</option>
+            </select>
+        </div>
+    )
+}
+
+export default Dpopdown2
