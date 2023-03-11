c0e1b6182845f824e695f78e18b3f5a630cc16d8
vohidjon7 <118883647+vohidjon7@users.noreply.github.com>
2023-03-11 19:10:23 +0500

    regisret

--- src/register/Login.jsx
+++ src/register/Login.jsx
@@ -1,22 +1,32 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
 import './register.css'
 
 function Login(props) {
-    const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [password2, setPassword2] = useState('')
-    const [check, setChek] = useState(false)
+    const navigate = useNavigate();
+    const [data, setData] = useState()
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!email || !password) {
+            window.alert("Formani to'ldiring")
+        } else {
+            setData({ email: email, password: password})
+            console.log(data);
+            navigate('/')
+        }
+    }
+    // const [check, setChek] = useState(false)
     return (
         <div className='register'>
             <div className='quti'>
                 <div>
                     <h3>Hisobingizga kiring.</h3>
                 </div>
-                <form className='form'>
-                    <input type="email" className='inp form-control' placeholder="Elektron pochta" onChange={(e) => { setEmail(e.target.value) }} />
-                    <input type="password" className='inp form-control' placeholder="Parol" onChange={(e) => { setPassword(e.target.value) }} />
+                <form className='form' onSubmit={handleSubmit}>
+                    <input type="email" className='inp form-control' value={email} placeholder="Elektron pochta" onChange={(e) => { setEmail(e.target.value) }} />
+                    <input type="password" className='inp form-control' value={password} placeholder="Parol" onChange={(e) => { setPassword(e.target.value) }} />
                     <div className='a mt-4'>
                         <label className="aiz-checkbox label">
                             <input type="checkbox" required="" />
@@ -25,30 +35,30 @@ function Login(props) {
                         </label>
                         <span className="opacity-60">Parolni unutdingizmi</span>
                     </div>
-                    <button className='btn btn-danger'>Kirish</button>
+                    <button className='btn btn-danger' onClick={handleSubmit}>Kirish</button>
                 </form>
-                <div class="separator mt-5 mb-3">
-                    <span class="bg-white px-3 opacity-60">Yoki kiring</span>
+                <div className="separator mt-5 mb-3">
+                    <span className="bg-white px-3 opacity-60">Yoki kiring</span>
                 </div>
-                <ul class="list-inline social colored text-center mb-5">
-                    <li class="list-inline-item">
-                        <a href="https://unimart.uz/social-login/redirect/facebook" class="facebook">
-                            <i class="i fa-brands fa-facebook"></i>
+                <ul className="list-inline social colored text-center mb-5">
+                    <li className="list-inline-item">
+                        <a href="/login" className="facebook">
+                            <i className="i fa-brands fa-facebook"></i>
                         </a>
                     </li>
-                    <li class="list-inline-item">
-                        <a href="https://unimart.uz/social-login/redirect/google" class="google">
-                            <i class="i fa-brands fa-google"></i>
+                    <li className="list-inline-item">
+                        <a href="/login" className="google">
+                            <i className="i fa-brands fa-google"></i>
                         </a>
                     </li>
-                    <li class="list-inline-item">
-                        <a href="https://unimart.uz/social-login/redirect/twitter" class="twitter">
-                            <i class="i fa-brands fa-twitter"></i>
+                    <li className="list-inline-item">
+                        <a href="/login" className="twitter">
+                            <i className="i fa-brands fa-twitter"></i>
                         </a>
                     </li>
                 </ul>
-                <div class="text-center">
-                    <p class="text-muted mb-0">Sizda hisob raqamingiz yo'qmi?</p>
+                <div className="text-center">
+                    <p className="text-muted mb-0">Sizda hisob raqamingiz yo'qmi?</p>
                     <Link to={"/register"} className='text'>
                         Roʻyxatdan oʻting
                     </Link>
--- src/register/Register.jsx
+++ src/register/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {Link} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import './register.css'
 
 function Register(props) {
@@ -7,47 +7,58 @@ function Register(props) {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [password2, setPassword2] = useState('')
-    const [check, setChek] = useState(false)
+    const navigate = useNavigate();
+    const [data, setData] = useState()
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!name || !email || !password || !password) {
+            window.alert("Formani to'ldiring")
+        } else {
+            setData({ name: name, email: email, password: password, password2: password2 })
+            console.log(data);
+            navigate('/')
+        }
+    }
     return (
         <div className='register'>
             <div className='quti'>
                 <div>
                     <h3>Ro'yxatdan o'tish</h3>
                 </div>
-                <form className='form'>
-                    <input type="text" className='inp form-control' value={name} placeholder="To'liq ismi sharif" onChange={(e) => { setName(e.target.value) }} />
-                    <input type="email" className='inp form-control' placeholder="Elektron pochta" onChange={(e) => { setEmail(e.target.value) }} />
-                    <input type="password" className='inp form-control' placeholder="Parol" onChange={(e) => { setPassword(e.target.value) }} />
-                    <input type="text" className='inp form-control' placeholder="Parolni tasdiqlang" onChange={(e) => { setPassword2(e.target.value) }} />
+                <form className='form' onSubmit={handleSubmit}>
+                    <input type="text" className='inp form-control' value={name} placeholder="To'liq ismi sharif" onChange={(e) => { setName(e.target.value) }} required />
+                    <input type="email" className='inp form-control' value={email} placeholder="Elektron pochta" onChange={(e) => { setEmail(e.target.value) }} required />
+                    <input type="password" className='inp form-control' value={password} placeholder="Parol" onChange={(e) => { setPassword(e.target.value) }} required />
+                    <input type="password" className='inp form-control' value={password2} placeholder="Parolni tasdiqlang" onChange={(e) => { setPassword2(e.target.value) }} required />
                     <label className="label">
-                        <input type="checkbox" required="" />
+                        <input type="checkbox" required />
                         <span className="opacity-60 mt-4">Ro'yxatdan o'tish orqali siz bizning shartlarimizga rozilik bildirasiz.</span>
                         <span className="aiz-square-check"></span>
                     </label>
-                    <button className='btn btn-danger'>Ro'yxatdan o'tish</button>
+                    <button className='btn btn-danger' onClick={handleSubmit}>Ro'yxatdan o'tish</button>
                 </form>
-                <div class="separator mt-5 mb-3">
-                    <span class="bg-white px-3 opacity-60">Yoki qo'shiling</span>
+                <div className="separator mt-5 mb-3">
+                    <span className="bg-white px-3 opacity-60">Yoki qo'shiling</span>
                 </div>
-                <ul class="list-inline social colored text-center mb-5">
-                    <li class="list-inline-item">
-                        <a href="https://unimart.uz/social-login/redirect/facebook" class="facebook">
-                        <i class="i fa-brands fa-facebook"></i>
+                <ul className="list-inline social colored text-center mb-5">
+                    <li className="list-inline-item">
+                        <a href="/" className="facebook">
+                            <i className="i fa-brands fa-facebook"></i>
                         </a>
                     </li>
-                    <li class="list-inline-item">
-                        <a href="https://unimart.uz/social-login/redirect/google" class="google">
-                        <i class="i fa-brands fa-google"></i>
+                    <li className="list-inline-item">
+                        <a href="/" className="google">
+                            <i className="i fa-brands fa-google"></i>
                         </a>
                     </li>
-                    <li class="list-inline-item">
-                        <a href="https://unimart.uz/social-login/redirect/twitter" class="twitter">
-                        <i class="i fa-brands fa-twitter"></i>
+                    <li className="list-inline-item">
+                        <a href="/" className="twitter">
+                            <i className="i fa-brands fa-twitter"></i>
                         </a>
                     </li>
                 </ul>
-                <div class="text-center">
-                    <p class="text-muted mb-0">Allaqachon hisobingiz bormi</p>
+                <div className="text-center">
+                    <p className="text-muted mb-0">Allaqachon hisobingiz bormi</p>
                     <Link to={"/login"} className='text'>
                         Kirish
                     </Link>
