0b45b60814e2f925ee91998749f0fa33a996140c
vohidjon7 <118883647+vohidjon7@users.noreply.github.com>
2023-03-12 22:47:04 +0500

    register

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import Login from './register/Login';
 import Navbar from './componets/Navbar';
 import Password from './register/Password';
 import Seller from './seller/Seller';
+import SellerDashboard from './seller/SellerDashboard';
+import CreateProduct from './seller/CreateProduct';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar/>
       <Routes>
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
         <Route path='/password' element={<Password />} />
         <Route path='/seller' element={<Seller />} />
+        <Route path='/seller/dashboard' element={<SellerDashboard />} />
+        <Route path='/product/create' element={<CreateProduct />} />
       </Routes>
     </div>
   );
--- src/seller/Seller.jsx
+++ src/seller/Seller.jsx
@@ -1,24 +1,37 @@
 import React, { useState } from 'react';
 import './seller.css'
 import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios';
 
 function Seller(props) {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [password2, setPassword2] = useState('')
-    const [dokon, setDokon] = useState('')
     const [manzil, setManzil] = useState('')
     const navigate = useNavigate();
-    const [data, setData] = useState()
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        if (!name || !email || !password || !password || !dokon || !manzil) {
-            window.alert("Formani to'ldiring")
+        if (password.length < 8) {
+            window.alert("Parol 8 ta belgidan kam bo'lmasin")
         } else {
-            setData({ name: name, email: email, password: password, password2: password2, dokon: dokon, manzil: manzil })
-            console.log(data);
-            navigate('/')
+            let data1 = {
+                name: name,
+                email: email,
+                password: password,
+                address: manzil
+            }
+            console.log(data1);
+            try {
+                let { data } = await axios.post('https://unimart-fast.up.railway.app/api/v1/seller/register', data1)
+                if (data.msg) {
+                    window.alert(data.msg)
+                } else {
+                    localStorage.setItem('userinfo',JSON.stringify(data));
+                    navigate('/seller/dashboard')
+                }
+            } catch (error) {
+                console.log(error);
+            }
         }
     }
     return (
@@ -36,7 +49,7 @@ function Seller(props) {
                     </div>
                     <form onSubmit={handleSubmit} className="p-3">
                         <div className="form-group mt-2">
-                            <label>Ismingiz <span className="text-primary">*</span></label>
+                            <label>Do'kon nomi <span className="text-primary">*</span></label>
                             <input type="text" className="form-control" placeholder="Nom" value={name} onChange={(e) => { setName(e.target.value) }} />
                         </div>
                         <div className="form-group mt-2">
@@ -48,23 +61,8 @@ function Seller(props) {
                             <input type="password" className="form-control" placeholder="Parol" value={password} onChange={(e) => { setPassword(e.target.value) }} />
                         </div>
                         <div className="form-group mt-2 mb-2">
-                            <label>Parolni qaytadan kiriting <span className="text-primary">*</span></label>
-                            <input type="password" className="form-control" placeholder="Parolni tasdiqlang" value={password2} onChange={(e) => { setPassword2(e.target.value) }} />
-                        </div>
-                    </form>
-                </div>
-                <div className="quti1 bg-white rounded shadow-sm mb-4">
-                    <div className="fs-15 fw-600 p-3 border-bottom">
-                        Asosiy ma'lumotlar
-                    </div>
-                    <form onSubmit={handleSubmit} className="p-3">
-                        <div className="form-group">
-                            <label>Do'kon nomi <span className="text-primary">*</span></label>
-                            <input type="text" className="form-control" placeholder="Do'kon nomi" value={dokon} required onChange={(e) => { setDokon(e.target.value) }} />
-                        </div>
-                        <div className="form-group">
                             <label>Manzil <span className="text-primary">*</span></label>
-                            <input type="text" className="form-control mb-3" placeholder="Manzil" value={manzil} required onChange={(e) => { setManzil(e.target.value) }} />
+                            <input type="text" className="form-control" placeholder="Manzil" value={manzil} onChange={(e) => { setManzil(e.target.value) }} />
                         </div>
                     </form>
                 </div>
